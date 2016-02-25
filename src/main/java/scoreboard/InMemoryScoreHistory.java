13a35bcc6b79dbe18cf07420675af344d91090ad
Johannes Link <business@johanneslink.net>
2016-02-25 15:08:49 +0100

    InMemoryScoreHistory can save, pop and clear.

--- src/main/java/scoreboard/InMemoryScoreHistory.java
+++ src/main/java/scoreboard/InMemoryScoreHistory.java
@@ -1,13 +1,44 @@
+
 package scoreboard;
 
-public class InMemoryScoreHistory implements ScoreHistory{
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class InMemoryScoreHistory implements ScoreHistory {
+
+	private Stack<Score> scores = new Stack<>();
+
 	@Override
 	public void save(int scoreA, int scoreB) {
-
+		scores.push(new Score(scoreA, scoreB));
 	}
 
 	@Override
 	public int[] pop() {
-		return new int[] {0, 0};
+		try {
+			return scores.pop().toIntArray();
+		}
+		catch (EmptyStackException ignore) {
+			return ZERO_SCORE;
+		}
+	}
+
+	public void clear() {
+		scores.clear();
+	}
+
+	private static class Score {
+
+		private final int scoreA;
+		private final int scoreB;
+
+		private Score(int scoreA, int scoreB) {
+			this.scoreA = scoreA;
+			this.scoreB = scoreB;
+		}
+
+		private int[] toIntArray() {
+			return new int[] { scoreA, scoreB };
+		}
 	}
 }
