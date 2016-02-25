f65a24ae4169d638637e270b049a2e8af8b2bfcc
Johannes Link <business@johanneslink.net>
2016-02-25 12:04:23 +0100

    console now takse scanner on start

--- src/main/java/scoreboard/ScoreboardConsole.java
+++ src/main/java/scoreboard/ScoreboardConsole.java
@@ -1,6 +1,6 @@
+
 package scoreboard;
 
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
@@ -8,16 +8,25 @@ public class ScoreboardConsole {
 	private final PrintWriter writer;
 	private final Scoreboard scoreboard;
 
+	public static void main(String[] args) {
+		ScoreboardConsole console = new ScoreboardConsole(new PrintWriter(System.out), new Scoreboard());
+		console.start(new Scanner(System.in));
+	}
+
 	public ScoreboardConsole(PrintWriter writer, Scoreboard scoreboard) {
 		this.writer = writer;
 		this.scoreboard = scoreboard;
 	}
 
-	public void start() {
+	public void start(Scanner scanner) {
 		showScore();
+		while(scanner.hasNext()) {
+
+		}
 	}
 
 	private void showScore() {
 		writer.println(String.format("%03d:%03d", scoreboard.scoreTeamA(), scoreboard.scoreTeamB()));
+		writer.flush();
 	}
 }
--- src/test/java/scoreboard/ScoreboardConsoleTest.java
+++ src/test/java/scoreboard/ScoreboardConsoleTest.java
@@ -1,29 +1,23 @@
+
 package scoreboard;
 
 import static org.junit.gen5.api.Assertions.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Scanner;
 
 import org.junit.gen5.api.Test;
-import org.junit.gen5.api.extension.ExtendWith;
-import org.mockito.Mockito;
-
-import junit5.helpers.InjectMock;
-import junit5.helpers.MockitoExtension;
 
 class ScoreboardConsoleTest {
 
+	StringWriter stringWriter = new StringWriter();
+	PrintWriter writer = new PrintWriter(stringWriter);
+	ScoreboardConsole console = new ScoreboardConsole(writer, new Scoreboard());
+
 	@Test
 	void startingConsoleShowsInitialScore() throws Exception {
-		StringWriter stringWriter = new StringWriter();
-		PrintWriter writer = new PrintWriter(stringWriter);
-		ScoreboardConsole console = new ScoreboardConsole(writer, new Scoreboard());
-		console.start();
+		console.start(new Scanner(""));
 		assertEquals("000:000" + System.lineSeparator(), stringWriter.toString());
 	}
 }
