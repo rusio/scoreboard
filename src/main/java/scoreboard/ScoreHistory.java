e1e3e9ed8c62728ce0ceaf3a427d26b74881de4a
Johannes Link <business@johanneslink.net>
2016-02-25 15:08:49 +0100

    InMemoryScoreHistory can save, pop and clear.

--- src/main/java/scoreboard/ScoreHistory.java
+++ src/main/java/scoreboard/ScoreHistory.java
@@ -1,6 +1,8 @@
 package scoreboard;
 
 public interface ScoreHistory {
+	int[] ZERO_SCORE = new int[] { 0, 0 };
+
 	void save(int scoreA, int scoreB);
 
 	int[] pop();
--- src/test/java/scoreboard/InMemoryScoreHistoryTests.java
+++ src/test/java/scoreboard/InMemoryScoreHistoryTests.java
@@ -1,16 +1,38 @@
 package scoreboard;
 
-import org.junit.gen5.api.Assertions;
 import org.junit.gen5.api.Test;
 
 import junit5.helpers.ArrayAssertions;
 
 class InMemoryScoreHistoryTests {
 
+	InMemoryScoreHistory history = new InMemoryScoreHistory();
+
 	@Test
 	void emptyHistoryWillReturnZeroScore() {
-		InMemoryScoreHistory history = new InMemoryScoreHistory();
-		ArrayAssertions.assertArrayEquals(new int[] {0, 0}, history.pop());
+		ArrayAssertions.assertArrayEquals(ScoreHistory.ZERO_SCORE, history.pop());
 	}
 
+	@Test
+	void savedScoreCanBePopped() {
+		history.save(19, 22);
+		ArrayAssertions.assertArrayEquals(new int[] { 19, 22 }, history.pop());
+	}
+
+	@Test
+	void manySavedScoresWillBePoppedInReverseOrder() {
+		history.save(1, 11);
+		history.save(2, 22);
+		history.save(3, 33);
+		ArrayAssertions.assertArrayEquals(new int[] { 3, 33 }, history.pop());
+		ArrayAssertions.assertArrayEquals(new int[] { 2, 22 }, history.pop());
+		ArrayAssertions.assertArrayEquals(new int[] { 1, 11 }, history.pop());
+	}
+
+	@Test
+	void afterClearHistoryWillReturnZeroScore() {
+		history.save(1, 11);
+		history.clear();
+		ArrayAssertions.assertArrayEquals(ScoreHistory.ZERO_SCORE, history.pop());
+	}
 }
