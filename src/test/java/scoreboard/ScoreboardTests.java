f2d0083d4e98f47a18679b2158fb48c402364ad2
Johannes Link <business@johanneslink.net>
2016-02-25 09:53:16 +0100

    initial score test

--- src/test/java/scoreboard/ScoreboardTests.java
+++ src/test/java/scoreboard/ScoreboardTests.java
@@ -1,14 +1,18 @@
+
 package scoreboard;
 
 import static org.junit.gen5.api.Assertions.*;
 
-import org.junit.gen5.api.Assertions;
 import org.junit.gen5.api.Test;
 
 class ScoreboardTests {
 
 	@Test
-	void aNewBoard() throws Exception {
-		assertNotNull(new Scoreboard());
+	void aNewBoard() {
+		Scoreboard board = new Scoreboard();
+		assertAll("current score", //
+			() -> assertEquals(0, board.scoreTeamA(), "team A"), //
+			() -> assertEquals(0, board.scoreTeamB(), "team B") //
+		);
 	}
 }
