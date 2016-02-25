8599cf5ffb477e90a56433ae4547c5fc65619b6e
Johannes Link <business@johanneslink.net>
2016-02-25 12:56:20 +0100

    score a and b can be increased

--- src/main/java/scoreboard/Scoreboard.java
+++ src/main/java/scoreboard/Scoreboard.java
@@ -1,11 +1,27 @@
+
 package scoreboard;
 
 public class Scoreboard {
+
+	private ScoreDisplay display;
+
+	private int scoreA = 0;
+	private int scoreB = 0;
+
 	public void registerDisplay(ScoreDisplay display) {
-		display.displayScore(0, 0);
+		display.displayScore(scoreA, scoreB);
+		this.display = display;
 	}
 
-	public void send(Command incA) {
-
+	public void send(Command command) {
+		switch (command) {
+			case INC_A:
+				scoreA++;
+				break;
+			case INC_B:
+				scoreB++;
+				break;
+		}
+		display.displayScore(scoreA, scoreB);
 	}
 }
