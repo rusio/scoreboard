85c0cc5143f7bc1f44e1321de575352852fd6c1c
Johannes Link <business@johanneslink.net>
2016-02-25 10:52:39 +0100

    team b plus now works

--- src/main/java/scoreboard/Scoreboard.java
+++ src/main/java/scoreboard/Scoreboard.java
@@ -3,16 +3,21 @@ package scoreboard;
 public class Scoreboard {
 
 	private int scoreTeamA = 0;
+	private int scoreTeamB = 0;
 
 	public int scoreTeamA() {
 		return scoreTeamA;
 	}
 
 	public int scoreTeamB() {
-		return 0;
+		return scoreTeamB;
 	}
 
 	public void plusTeamA() {
 		scoreTeamA++;
+	}
+
+	public void plusTeamB() {
+		scoreTeamB++;
 	}
 }
