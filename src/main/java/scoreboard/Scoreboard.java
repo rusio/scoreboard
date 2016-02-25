6781086ba5285d94a7e686309d9af79ec8e587e9
Johannes Link <business@johanneslink.net>
2016-02-25 12:48:01 +0100

    added test for increasing team A score

--- src/main/java/scoreboard/Scoreboard.java
+++ src/main/java/scoreboard/Scoreboard.java
@@ -4,4 +4,8 @@ public class Scoreboard {
 	public void registerDisplay(ScoreDisplay display) {
 		display.displayScore(0, 0);
 	}
+
+	public void send(Command incA) {
+
+	}
 }
--- src/test/java/scoreboard/ScoreboardTests.java
+++ src/test/java/scoreboard/ScoreboardTests.java
@@ -6,17 +6,30 @@ import static org.mockito.Mockito.verify;
 import junit5.helpers.InjectMock;
 import junit5.helpers.MockitoExtension;
 
+import org.junit.gen5.api.BeforeEach;
 import org.junit.gen5.api.Test;
 import org.junit.gen5.api.extension.ExtendWith;
+import org.mockito.Mockito;
 
 @ExtendWith(MockitoExtension.class)
 class ScoreboardTests {
 
+	Scoreboard board = new Scoreboard();
+
 	@Test
 	void registeringADisplayWillDisplayCurrentScore(@InjectMock ScoreDisplay display) {
-		Scoreboard board = new Scoreboard();
 		board.registerDisplay(display);
 
 		verify(display).displayScore(0, 0);
+	}
+
+	@Test
+	void teamAPlusWillDisplayIncreasedScore(@InjectMock ScoreDisplay display) {
+		board.registerDisplay(display);
+		Mockito.reset(display);
+
+		board.send(Command.INC_A);
+
+		verify(display).displayScore(1, 0);
 	}
 }
